7d8e082facc8d129d427a84ade9bc25720d47e07
DESKTOP-4NP9D3F\Tintin <tristankuisis1@gmail.com>
2018-10-15 21:47:53 +0200

    Fixed formatting for report, almost finished introductory sections, started on the frequency choice. Did calculations for the velocities of things.

--- Calculations/Radar Equations/Calculations.py
+++ Calculations/Radar Equations/Calculations.py
@@ -17,24 +17,45 @@ Lambda = speedOfLight / centerFrequency
 # Tangential velocity
 minObjectPeriod = 5280 # Seconds
 maxObjectPeriod = 7620 # Seconds
-maxObjectSpeed = (2 * math.pi * (earthRadius+minDistance))/(minObjectPeriod)
-test = math.sqrt((gravitationalConstant * earthMass)/(earthRadius+minDistance))
+# maxObjectSpeed = (2 * math.pi * (earthRadius+minDistance))/(minObjectPeriod)
+maxObjectSpeed = math.sqrt((gravitationalConstant * earthMass)/(earthRadius+minDistance))
+minObjectSpeed = math.sqrt((gravitationalConstant * earthMass)/(earthRadius+maxDistance))
 
 # Maximum Angle (from zenith)
-maxAngle = 60 * (math.pi / 180)
+maxAngle = 30 * (math.pi / 180)
 
-# Max doppler shift
-dopplerFrequencyShift = 2*((maxObjectSpeed)/(speedOfLight))*centerFrequency 
 
+
+# Slant Distance
 elevation = (math.pi)/2 - maxAngle
-targetDistance = earthRadius * (math.sqrt((((earthRadius + maxDistance)**2)/(earthRadius**2)) - (math.cos(elevation)**2 )) - math.sin(elevation))
-maxTargetDistance = earthRadius * (math.sqrt((((earthRadius + maxDistance)**2)/(earthRadius**2)) - (math.cos((math.pi/2 )-(math.pi/3))**2 )) - math.sin((math.pi/2 )-(math.pi/3)))
+maxSlantDistance = earthRadius * (math.sqrt((((earthRadius + maxDistance)**2)/(earthRadius**2)) - (math.cos(elevation)**2 )) - math.sin(elevation)) # Taking in the max angle (from zenith) and max distance
+minSlantDistance = earthRadius * (math.sqrt((((earthRadius + minDistance)**2)/(earthRadius**2)) - (math.cos(elevation)**2 )) - math.sin(elevation)) # Taking in the max angle (from zenith) and min distance
+
+maxSlantDistanceFOV = earthRadius * (math.sqrt((((earthRadius + maxDistance)**2)/(earthRadius**2)) - (math.cos((math.pi/2 )-(math.pi/3))**2 )) - math.sin((math.pi/2 )-(math.pi/3))) # Assuming 120 degrees FOV
+
 # Path Loss of signal (one direction)
-pathLoss = (Lambda/(4 * math.pi * targetDistance))
+pathLoss = (Lambda/(4 * math.pi * maxSlantDistance))
 
 
 # Observable Time
-observableTime = (((earthRadius + maxDistance)**(3.0/2.0))/(math.sqrt(gravitationalConstant * earthMass))) * (math.pi - 2 * elevation - 2 * math.asin((earthRadius)/(earthRadius + maxDistance) * math.cos(elevation)))
+maxObservableTime = (((earthRadius + maxDistance)**(3.0/2.0))/(math.sqrt(gravitationalConstant * earthMass)))*(math.pi - 2 * elevation - 2 * math.asin((earthRadius)/(earthRadius + maxDistance) * math.cos(elevation)))
+
+
+# Angular Velocity
+maxAngularVelocity = (maxObjectSpeed)/(minDistance)
+minAngularVelocity = (minObjectSpeed)/(maxDistance)
+
+
+# Apparent Angular Velocity
+apparentAngularVelicity = (2 * maxAngle)/(maxObservableTime)
+
+# Linear Velocity (measured from center of earth of from system)
+minLinearVelocity = apparentAngularVelicity * minDistance
+maxLinearVelocity = apparentAngularVelicity * maxDistance
+
+# Max doppler shift
+dopplerFrequencyShift = 2*((maxLinearVelocity)/(speedOfLight))*centerFrequency 
+
 
 # Minimum radar cross section of object (min diameter of 10 cm)
 minObjectRadius = 10 * 10**-2
@@ -42,13 +63,12 @@ minRCS = math.pi * minObjectRadius**2
 
 # Signal Round trip times
 minRoundTripTime = 2 * (minDistance/speedOfLight)
-maxRoundTripTime = 2 * (maxTargetDistance/speedOfLight)
+maxRoundTripTime = 2 * (maxSlantDistance/speedOfLight)
 
 # Pulse Width and Range Resolution Limits
 maxBandwidth = 8 * 10**6
 pulseWidthMin = 1/maxBandwidth
 pulseWidthMax = minRoundTripTime
-print(pulseWidthMin)
 rangeResolutionMin = (speedOfLight * pulseWidthMin)/(2)
 
 
@@ -57,13 +77,12 @@ rangeResolutionMin = (speedOfLight * pulseWidthMin)/(2)
 # pulseWidth = (2*rangeResolution)/(speedOfLight)
 
 
-print(rangeResolutionMin)
 
 # Thermal Noise
 noiseFigure = 7.94 # This should be calculated
 bandwidth = 1/pulseWidthMin
 thermalNoise = boltzmannsConstant * 290 * noiseFigure * bandwidth
-print(thermalNoise)
+
 # print(bandwidth/(1000000))
 
 
